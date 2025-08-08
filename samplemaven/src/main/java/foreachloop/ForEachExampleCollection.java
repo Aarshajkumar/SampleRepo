851733d87cfee5153f21b15851e1e1e38b6af317
Dell <Dell@DESKTOP-RVE60IM>
2025-08-08 11:31:20 +0530

    Foreach Commit

---
+++ samplemaven/src/main/java/foreachloop/ForEachExampleCollection.java
@@ -0,0 +1,18 @@
+package foreachloop;
+
+import java.util.ArrayList;
+
+public class ForEachExampleCollection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ArrayList<String> a = new ArrayList<String>();
+a.add("Red");
+a.add("Yellow");
+a.add("Blue");
+for(String b:a) {
+	System.out.println(b);
+}
+	}
+
+}
